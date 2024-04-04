afb83ab3c84634d89e2a808fc51d45d97eaf75fd
MauricioTejada <mauriciotejada14@gmail.com>
2024-04-04 10:34:31 -0300

    add 'Layout' component

---
+++ src/assets/backgrounds/index.js
@@ -0,0 +1,3 @@
+export { default as backgroundHome } from './planisferio3.jpg'
+export { default as backgroundCreateActivity } from './planisferio5.png'
+export { default as backgroundDetail } from './planisferio4.jpg'
